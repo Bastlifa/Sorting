bb21be131d6194fddb49cd35b6938c10535a0d25
Eli Sacks <bastlifa@gmail.com>
2019-12-10 17:02:45 -0500

    works? I can't wrap my head around it

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,27 +3,45 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
     for i in range(len(merged_arr)):
-        if len(arrA) == 0 or arrB[0] < arrA[0]:
+        if len(arrA) == 0:
             merged_arr[i] = arrB[0]
             arrB.pop(0)
-        else:
+        elif len(arrB) == 0:
             merged_arr[i] = arrA[0]
             arrA.pop(0)
-        print(merged_arr)
+        elif arrA[0] <= arrB[0]:
+            merged_arr[i] = arrA[0]
+            arrA.pop(0)
+        else:
+            merged_arr[i] = arrB[0]
+            arrB.pop(0)
+    
     return merged_arr
 
-print(merge([1,3,6,7], [2, 2, 9]))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) == 1: 
+        return arr
+    if len(arr) == 0:
+        return arr
 
+    left = arr[:len(arr)//2]
+    right = arr[len(arr)//2:]
+
+    left = merge_sort(left) or left
+    right = merge_sort(right) or right
+
+    arr = merge(left, right)
+    
     return arr
 
+# print(merge_sort([4, 2, 3, 1, 0, 9, 5, 4, 2, 0, 1, 8, 7]))
 
 # STRETCH: implement an in-place merge sort algorithm
+# pass on that
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
